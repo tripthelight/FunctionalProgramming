cec069480a160c563e14e6756b5e860544db62f1
tripthelight <bye_ok@naver.com>
2025-06-17 16:37:10 +0900

    [250617] - code init

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ const log = console.log;
  * 명령형 코드
  */
 // 리스트에서 홀수를 length 만큼 봅아서 제곱한 후 모두 더하기
-function fForLoop(list, length) {
-  log(list);
+function fForLoop(iter, length) {
+  log(iter);
 }
 
 function mainForLoop() {
+  fForLoop([1, 2, 3, 4, 5], 1);
   fForLoop([1, 2, 3, 4, 5], 2);
+  fForLoop([1, 2, 3, 4, 5], 3);
 }
 
 mainForLoop();
@@ -21,7 +23,7 @@ mainForLoop();
  */
 
 function mainFp() {
-  // log(f1([1, 2, 3, 4, 5], 2));
+  //
 }
 
 mainFp();
