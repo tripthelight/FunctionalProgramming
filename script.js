d624a02b713fd83e9f42c0d4b7388615855228e8
tripthelight <bye_ok@naver.com>
2025-06-19 15:17:51 +0900

    [250629] - 2차원 배열

--- script.js
+++ script.js
@@ -6,7 +6,15 @@ const log = console.log;
  */
 // 리스트에서 홀수를 length 만큼 봅아서 제곱한 후 모두 더하기
 function fForLoop(list, length) {
-  //
+  let i = 0;
+  let res = 0;
+  for (const a of list) {
+    if (a % 2) {
+      res = res + a * a;
+      if (++i == length) break;
+    }
+  }
+  log(res);
 }
 
 function mainForLoop() {
@@ -15,15 +23,151 @@ function mainForLoop() {
   fForLoop([1, 2, 3, 4, 5], 3);
 }
 
-mainForLoop();
+// mainForLoop();
 
 /**
  * *************************************
  * 함수형 코드
  */
+const L = {};
+
+L.range = function* (stop) {
+  let i = -1;
+  while(++i < stop) yield i;
+}
+
+const curry = (f) => (a, ...bs) => bs.length ? f(a, ...bs) : (...bs) => f(a, ...bs);
+
+L.filter = curry(function* (f, iter) {
+  for (const a of iter) {
+    if (f(a)) yield a;
+  }
+})
+
+L.map = curry(function* (f, iter) {
+  for (const a of iter) {
+    yield f(a);
+  }
+})
+
+const take = curry(function (length, iter) {
+  let res = [];
+  for (const a of iter) {
+    res.push(a);
+    if (res.length == length) return res;
+  }
+  return res;
+})
+
+const reduce = curry(function (f, acc, iter) {
+  if (arguments.length == 2) {
+    iter = acc[Symbol.iterator]();
+    acc = iter.next().value;
+  }
+  for (const a of iter) {
+    acc = f(acc, a);
+  }
+  return acc;
+})
+
+const add = curry((a, b) => a + b);
+
+// log(' ///////////////////////////// ');
+// log(reduce(add, 10, [10, 20, 30]));
+// log(reduce(add, [10, 20, 30]));
+// log(' ///////////////////////////// ');
+
+const go = (...fs) => reduce((a, f) => f(a), fs);
+
+// log(' ///////////////////////////// ');
+// go(10, a => a + 10, a => a + 1, log);
+// log(' ///////////////////////////// ');
+
+const fFp = (list, length) =>
+  reduce(add, 0,
+    take(length,
+      L.map(a => a * a,
+        L.filter(a => a % 2, list))));
+
+const fGo = (list, length) =>
+  go(list,
+    L.filter(a => a % 2),
+    L.map(a => a * a),
+    take(length),
+    reduce(add));
 
 function mainFp() {
-  //
+  log(fGo([1, 2, 3, 4, 5], 1));
+  log(fGo([1, 2, 3, 4, 5], 2));
+  log(fGo([1, 2, 3, 4, 5], 3));
+  log(fGo(L.range(Infinity), 3));
 };
 
-mainFp();
+// mainFp();
+
+/**
+ * *************************************
+ * 2차원 배열
+ */
+const arr = [
+  [1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [9, 10],
+];
+
+L.flat = function* (iter) {
+  for (const a of iter) {
+    if (a && a[Symbol.iterator]) for (const b of a) yield b;
+    else yield a;
+  }
+}
+
+go(arr,
+  L.flat,
+  L.filter(a => a % 2),
+  L.map(a => a * a),
+  take(3),
+  reduce(add),
+  log
+);
+
+/**
+ * *************************************
+ * 유저 목록
+ */
+const users = [
+  { name: 'a', age: 21, family: [
+    { name: 'a1', age: 53 },
+    { name: 'a2', age: 47 },
+    { name: 'a3', age: 16 },
+    { name: 'a4', age: 14 },
+  ] },
+  { name: 'b', age: 24, family: [
+    { name: 'b1', age: 58 },
+    { name: 'b2', age: 51 },
+    { name: 'b3', age: 10 },
+    { name: 'b4', age: 22 },
+  ] },
+  { name: 'c', age: 31, family: [
+    { name: 'c1', age: 64 },
+    { name: 'c2', age: 62 },
+  ] },
+  { name: 'd', age: 20, family: [
+    { name: 'd1', age: 42 },
+    { name: 'd2', age: 42 },
+    { name: 'd3', age: 11 },
+    { name: 'd4', age: 7 },
+  ] },
+];
+
+go(users,
+  L.map(u => u.family),
+  L.flat,
+  L.filter(u => u.age < 20),
+  L.map(u => u.age),
+  take(2),
+  reduce(add),
+  log,
+);
+console.clear();
