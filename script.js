190a770b785fb98c571cfeea8748b9233b339e85
tripthelight <bye_ok@naver.com>
2025-06-21 23:21:34 +0900

    [250621] - script.js init

--- script.js
+++ script.js
@@ -21,7 +21,6 @@ mainForLoop();
  * *************************************
  * 함수형 코드
  */
-
 function mainFp() {
 
 };
@@ -34,7 +33,7 @@ mainFp();
  */
 const arr = [
   [1, 2],
-  3, 4, 5,
+  [3, 4, 5],
   [6, 7, 8],
   [9, 10],
 ];
@@ -49,3 +48,27 @@ const users = [
   {name: 'c', age: 31, family: [{ name: 'c1', age: 64 }, { name: 'c2', age: 62 }]},
   {name: 'd', age: 20, family: [{ name: 'd1', age: 42 }, { name: 'd2', age: 42 }, { name: 'd3', age: 11 }, { name: 'd4', age: 7 }]},
 ];
+
+/**
+ * *************************************
+ * 아임포트
+ */
+const Impt = {
+  payments: {
+    0: [{ iid: 11, oid: 1 }, { iid: 12, oid: 2 }, { iid: 13, oid: 3 }],
+    1: [{ iid: 14, oid: 4 }, { iid: 15, oid: 5 }, { iid: 16, oid: 6 }],
+    2: [{ iid: 17, oid: 7 }, { iid: 18, oid: 8 }],
+    3: [],
+  },
+  getPayments: page => {
+    console.log(`http://...?page=${page}`);
+    return delay(100, Impt.payments[page]);
+  },
+  cancelPayment: paymentId => Promise.resolve(`${paymentId}: 취소 완료`)
+};
+
+const getOrders = ids => delay(100, [{ id: 1 }, { id: 3 }, { id: 7 }]);
+
+async function job() {
+
+}
