58004f19af7266645aa6eef9752bbd3e06b4e95f
tripthelight <bye_ok@naver.com>
2025-06-24 00:55:16 +0900

    [250624] - errorHandling init

--- errorHandling.js
+++ errorHandling.js
@@ -13,75 +13,18 @@ const imgs2 = [
   { name: "HEART", url: "https://i.pinimg.com/236x/f9/55/e5/f955e5f64c394627bc085d5f41762ee7.jpg" },
 ];
 
-const loadImage1 = url => new Promise((resolve, reject) => {
-  let img = new Image();
-  img.src = url;
-  // log('이미지 로드 ', url);
-  img.onload = function () {
-    resolve(img);
-  }
-  img.onerror = function (e) {
-    reject(e);
-  }
-  return img;
-});
-// loadImage(imgs[0].url).then(log);
+/**
+ * 실전
+  - 이미지들을 불러와서 모든 이미지의 높이를 더한다.
+ */
 
-async function f1() {
-  try {
-    let error = null;
-    const total = await imgs2
-      .map( async ({url}) => {
-        if (error) return;
-        try {
-          const img = await loadImage1(url);
-          return img.height;
-        } catch (e) {
-          error = e;
-          log(e);
-          throw e;
-        }
-      })
-      .reduce(async (total, height) => await total + await height, 0);
+function f1() {
   
-    log(total);
-  } catch (error) {
-    log(0);
-  }
 }
-// f1();
+  
+f1();
 
-// /////////////////////////////////////////
-const loadImage = url => new Promise((resolve, reject) => {
-  let img = new Image();
-  img.src = url;
-  // log('이미지 로드 ', url);
-  img.onload = function () {
-    resolve(img);
-  }
-  img.onerror = function (e) {
-    reject(e);
-  }
-});
-
-function* map(f, iter) {
-  for ( const a of iter ) {
-    log('a : ', a)
-    yield a instanceof Promise ? a.then(f) : f(a);
-  }
-}
-
-async function reduceAsync (f, acc, iter) {
-  for await ( const a of iter ) {
-    acc = f(acc, a);
-  }
-  return acc;
-}
-
-const f2 = (imgs) =>
-  reduceAsync((a, b) => a + b, 0,
-    map((img) => img.height,
-      map(({url}) => loadImage(url), imgs)));
-
-f2(imgs).catch(_ => 0).then(log);
-f2(imgs2).catch(_ => 0).then(log);
+/**
+ * 해결
+  - Promise, async/await, try/catch 잘다루기
+ */
