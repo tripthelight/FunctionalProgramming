63199088c3b4316aef01a04131a8eccbdc9482ab
tripthelight <bye_ok@naver.com>
2025-06-26 15:36:05 +0900

    [250626] - tmpl init

---
+++ tmpl.js
@@ -0,0 +1,17 @@
+const products = [
+  { name: '반팔티', price: 15000, quantity: 1, selected: true },
+  { name: '긴팔티', price: 20000, quantity: 2, selected: false },
+  { name: '핸드폰케이스', price: 15000, quantity: 3, selected: true },
+  { name: '후드티', price: 30000, quantity: 4, selected: false },
+  { name: '바지', price: 25000, quantity: 5, selected: false },
+  { name: '반팔티', price: 15000, quantity: 1, selected: true },
+  { name: '긴팔티', price: 20000, quantity: 2, selected: false },
+  { name: '핸드폰케이스', price: 15000, quantity: 3, selected: true },
+  { name: '후드티', price: 30000, quantity: 4, selected: false },
+  { name: '바지', price: 25000, quantity: 5, selected: false },
+  { name: '반팔티', price: 15000, quantity: 1, selected: true },
+  { name: '긴팔티', price: 20000, quantity: 2, selected: false },
+  { name: '핸드폰케이스', price: 15000, quantity: 3, selected: true },
+  { name: '후드티', price: 30000, quantity: 4, selected: false },
+  { name: '바지', price: 25000, quantity: 5, selected: false },
+];
