ed428c6e43632ad8f4f996ea91e8c8643c041784
tripthelight <bye_ok@naver.com>
2025-07-08 18:48:05 +0900

    [250708] - 큐처리기

--- PromiseAsnynAwait.js
+++ PromiseAsnynAwait.js
@@ -7,8 +7,8 @@
 // 요청이 OK가 아닐 시, 추가적으로 요청을 더 시킬 수 있다. - retryAsync
 // 요청이 OK가 아닐 시, 추가적으로 더 요청을 보낼 때 time^n 만큼 축척된 delay를 줄 수 있다. - retryAsync에 backoff 기능 추가
 // 요청 시작 후, 요청 대기 시간을 미리 정해서, 그 시간안에 응답이 오지 않으면 중지 시킬 수 있다. - AbortController
+// 요청 시작 후, 요청 대기 시간과 재요청 회수를 미리 정해서, 미리 설정한 대기시간보다 재요청 시간이 길어질 경우 중지 시킬 수 있다.
+// 큐처리기를 이용해서 동기, 비동기 함수들의 순서를 강제할 수 있다.
 
-const url = 'https://jsonplaceholder.typicode.com/posts/1';
-const url404 = 'https://jsonplaceholder.typicode.com/postss/1';
-const urlBad = 'https://dddjsonplaceholder.typicode.com/posts/1';
+
 
