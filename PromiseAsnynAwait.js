74f409f7c7e07f25abd35a67f7d11bd45f1890fb
tripthelight <bye_ok@naver.com>
2025-07-09 18:53:27 +0900

    [250709] - 큐처리기 abort 응용

--- PromiseAsnynAwait.js
+++ PromiseAsnynAwait.js
@@ -4,6 +4,10 @@
 // 클로져는 더 약하다 - 클로져로 구성된 코드를 전혀 분석하지 못한다.
 // ******************************************************
 
+const url = 'https://jsonplaceholder.typicode.com/posts/1';
+const url404 = 'https://jsonplaceholder.typicode.com/postss/1';
+const urlBad = 'https://dddjsonplaceholder.typicode.com/postss/1';
+
 // 요청이 OK가 아닐 시, 추가적으로 요청을 더 시킬 수 있다. - retryAsync
 // 요청이 OK가 아닐 시, 추가적으로 더 요청을 보낼 때 time^n 만큼 축척된 delay를 줄 수 있다. - retryAsync에 backoff 기능 추가
 // 요청 시작 후, 요청 대기 시간을 미리 정해서, 그 시간안에 응답이 오지 않으면 중지 시킬 수 있다. - AbortController
@@ -20,12 +24,9 @@
 
 // console.log(`요청 결과:\n ${JSON.stringify(result, null, 2).replace(/\\n/g, '\n')}`);
 
-const url = 'https://jsonplaceholder.typicode.com/posts/1';
-const url404 = 'https://jsonplaceholder.typicode.com/postss/1';
-const urlBad = 'https://dddjsonplaceholder.typicode.com/postss/1';
-
-
-
-
+/**
+다음은 retryAsync, backoff, concurrency 제한 등을 이 큐 처리기에 하나씩 덧붙이게 될 예정입니다.
+계속해서 진행하실까요? 아니면 잠깐 쉬거나 복습하고 싶으신가요? 😊
+ */
 
 
